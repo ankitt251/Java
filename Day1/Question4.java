761c50f519ddd9ee7a297ff5145ef83e03514634
ankitt251 <89956629+ankitt251@users.noreply.github.com>
2024-07-30 15:23:59 +0530

    Question4

---
+++ Day1/Question4.java
@@ -0,0 +1,20 @@
+package Day1;
+import java.util.Scanner;
+
+
+
+public class Question4 {
+    public static void avg(int a,int b,int c){
+        int total_marks = a + b +c;
+        int percentage = (total_marks)/ 3;
+        System.out.println(percentage);
+    }
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+    
+        avg(a,b,c);
+    }
+}
