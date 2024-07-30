bf8c035d61ccfdc844f4d75a9b639680b3a8e91b
ankitt251 <89956629+ankitt251@users.noreply.github.com>
2024-07-30 15:04:36 +0530

    Question3

---
+++ Day1/Question3.java
@@ -0,0 +1,16 @@
+package Day1;
+
+public class Question3 {
+    public static void result(){
+        int a = 2345;
+        int sum = a +8;
+        int div = sum / 3;
+        int mod = div % 5;
+        int mul = mod *5;
+        System.out.println(mul); 
+    }
+    public static void main(String args[]){
+        result();
+    
+    }
+}
